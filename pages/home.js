f4942ed6f1d7bae5bebfdefbd45bf0328f73d404
Tonny <tonnymakondesa6@gmail.com>
2023-04-10 12:26:56 +0200

    added functionality

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,29 @@
-import { StyleSheet, TextInput, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { StyleSheet, TextInput, Text, View, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native";
 
 const Home = () => {
+
+    const [info, setInfo] = useState({})
+
+    const [zip, setZip] = useState();
+    const [cCode, setcCode] = useState('')
+    console.log(zip)
+    
+
+    const func = (() => {
+        axios.get(`https://api.zippopotam.us/${cCode}/${zip}`).then((value)=>{
+            setInfo(value.data)
+        })
+      });
+
+    const keyboardDismiss = ()=>{
+        Keyboard.dismiss()
+    }
+
     return ( 
         <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={keyboardDismiss}>
             <View style={styles.input}>
                 <TextInput 
                      style={{
@@ -11,9 +32,10 @@ const Home = () => {
                          borderColor:'#000000',
                          borderWidth:1,
                          marginVertical:10,
-                         
                      }}
-                     placeholder='Enter Zipcode'
+                     placeholder=' Enter Zipcode'
+                     value={zip}
+                     onChangeText={(text) => setZip(text)}                     
                 />
                 <TextInput 
                      style={{
@@ -23,9 +45,26 @@ const Home = () => {
                          borderWidth:1,
                          marginVertical:10,
                      }}
-                     placeholder='Enter Country Code'
+                     placeholder=' Enter Country Code'
+                     value={cCode}
+                     onChangeText={(text) => setcCode(text)}
                 />
             </View>
+            </TouchableWithoutFeedback>
+            <View style={styles.output}>
+               <Text style={styles.txt}>Post Code:{info['post code']}</Text>
+               <Text style={styles.txt}>Country:{info.country}</Text>
+               <Text style={styles.txt}>State:</Text> 
+               <Text style={styles.txt}>State Code:</Text>
+               <Text style={styles.txt}>Country Abbrevition:</Text>
+               <Text style={styles.txt}>Longitude:</Text>
+               <Text style={styles.txt}>Latitude:</Text>   
+            </View>
+            <TouchableOpacity style={styles.touchable} onPress={func}>
+                <View style={styles.button}>
+                    <Text style={styles.text}>Search</Text>
+                </View>
+            </TouchableOpacity>
         </View>
      );
 }
@@ -37,6 +76,31 @@ const styles = StyleSheet.create({
     input:{
         display:'flex',
         alignItems:'center',
+    },
+    output:{
+        backgroundColor:'#9ed9f0',
+        height:250,
+        width:325,
+        marginHorizontal:30,
+        marginVertical:50,
+        borderRadius:5,
+    },
+    touchable:{
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    button:{
+        backgroundColor:'#9ed9f0',
+        width:325,
+        height:40,
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:5,
+    },
+    text:{
+
     }
 })
  
